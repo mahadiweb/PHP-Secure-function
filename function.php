86be697efaada20ac19a6b5e43a7877385556eb2
Md.Mahadi Hasan <64768414+mahadiweb@users.noreply.github.com>
2021-10-22 21:10:56 +0600

    Update function.php

--- function.php
+++ function.php
@@ -1,19 +1,19 @@
-1=>strip_tags();
+1=>strip_tags(); //strip_tags() function strips/remove a string from HTML, XML, and PHP tags.
 
-2=>mysqli_real_escape_string();
+2=>mysqli_real_escape_string(); //function escapes special characters in a string for use in an SQL query, taking into account the current character set of the connection.
 
-3=>htmlspecialchars();
+3=>htmlspecialchars(); //htmlspecialchars() function print value with html tag, not execute html tag.
 
-4=>trim();
+4=>trim();  //trim() function removes whitespace and other predefined characters from both sides of a string.
 
-5=>define();
+5=>define(); //define() function a useful function to use user can't direct access your file.
 
-6=>filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+6=>filter_var($id, FILTER_SANITIZE_NUMBER_INT); //Sanitize special characters from integer.
 
-7=>md5();
+7=>md5(); //generate hash.
 
-8=>sha1();
+8=>sha1(); //generate hash.
 
-9=>password_hash();
+9=>password_hash(); //generate hash.
 
-10=>error_reporting();
+10=>error_reporting(); //hide error from user.
